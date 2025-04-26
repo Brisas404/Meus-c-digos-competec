78cf74930acdb261436029f189d7a21b158ef6c4
Brisas404 <silvapedroka44@gmail.com>
2025-04-26 11:27:10 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+        
+      char PrimeiraLetra = 'P';
+	   System.out.println(PrimeiraLetra);
+	   
+	  int MinhaIdade = 15;
+	   System.out.println(MinhaIdade);
+	 
+	  boolean TomeiCafe = true;
+	   System.out.println(TomeiCafe);
+	  
+	   
+	 }
+	  
+}
