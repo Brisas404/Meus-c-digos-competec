108441eb064af964cfea4c441f6b9af1bd7abe75
Pedro.Hs <silvapedroka44@gmail.com>
2025-05-10 10:24:04 -0300

    Add files via upload

---
+++ Comparando_idade.java
@@ -0,0 +1,15 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	
+	 int MinhaIdade = 15;
+	 int ProfessorIdade = 20;
+	 
+	  Boolean Outraidade = MinhaIdade == ProfessorIdade;
+	  
+	  System.out.println("As idades são iguais? "+Outraidade);
+	  
+	  
+	}
+}
