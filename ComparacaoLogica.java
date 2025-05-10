53ba7102d7ba669a9625f177acfdfa0c6d16a823
Pedro.Hs <silvapedroka44@gmail.com>
2025-05-10 10:45:51 -0300

    Add files via upload

---
+++ ComparacaoLogica.java
@@ -0,0 +1,15 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	
+	 boolean Relampejando = false;
+	 boolean Chovendo = true;
+	 
+	 boolean NaoSaireiDeCasa = Chovendo && Relampejando;
+	 
+	  System.out.println(NaoSaireiDeCasa);
+	  
+	  
+	}
+}
