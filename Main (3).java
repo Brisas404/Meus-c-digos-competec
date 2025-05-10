1b65461d7d633f77322b621c6568e7701907787c
Pedro.Hs <silvapedroka44@gmail.com>
2025-05-10 10:16:31 -0300

    Add files via upload

---
+++ Main (3).java
@@ -0,0 +1,15 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	
+	 String Meunome = "Pedro";
+	 String Outronome = "Luan";
+	 
+	  Boolean Mesmonome = Meunome == Outronome;
+	  
+	  System.out.println("Os nomes são iguais? "+Mesmonome);
+	  
+	  
+	}
+}
