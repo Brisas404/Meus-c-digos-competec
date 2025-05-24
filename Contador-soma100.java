9714c0d4485646976175ef33e5e2b5b0385cdece
Pedro.Hs <silvapedroka44@gmail.com>
2025-05-24 10:23:16 -0300

    Add files via upload

---
+++ Contador-soma100.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	 Scanner sc = new Scanner(System.in);	
+	 
+		int x = 0;
+		int y = 0 ;
+		
+	   while (x<=100) {
+	     y = y + x;
+	     x++;
+	     System.out.println( y+ " ");
+	   }
+	   
+	   
+	}
+}
---
+++ While-Palavra.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	 Scanner sc = new Scanner(System.in);	
+	 
+		String P;
+		int x = 0;
+		
+	   System.out.println("Escreva uma Palavra \n");
+	   P = sc.nextLine();
+	   
+	   while (x<5) {
+	      System.out.println(P);
+	      x++;
+	   }
+	   
+	   
+	}
+}
